d332da42d9a1c9fab4b833e0b84b2c06d1f7d5a4
eduardocsimoes81 <eduardocsimoes81@gmail.com>
2017-09-22 16:32:58 -0300

    continuacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,27 @@ $( function() {
   $('.filterarea').find('input').on('change', function(){
     $('.filterarea form').submit();
   });
+
+  $('.addtocartform button').on('click', function(e){
+    e.preventDefault();
+
+    var qt = parseInt($('.addtocart_qt').val());
+    var action = $(this).attr('data-action');
+
+    if(action == 'decrease'){
+      if(qt-1 >= 1){
+        qt = qt - 1;
+      }
+    }else if(action == 'increase'){
+      qt = qt + 1;
+    }
+
+    $('.addtocart_qt').val(qt);
+  });
+
+  $('.photo_item').on('click', function(){
+    var url = $(this).find('img').attr('src');
+
+    $('.mainphoto').find('img').attr('src', url);
+  });
 });
